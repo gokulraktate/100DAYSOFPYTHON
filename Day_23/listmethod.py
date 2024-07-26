64064f38454496ae1e8b17265e409828fc526552
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-07-26 23:45:47 +0530

    List Methods

---
+++ Day_23/listmethod.py
@@ -0,0 +1,32 @@
+# List Methods in Python | Python Tutorial - Day #23
+
+# https://www.youtube.com/watch?v=scWc3F8LbOo&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=23
+
+
+l=[1,2,22,4,11,5,44,8,1]
+# print(l)
+
+# l.append(7)
+# print(l)
+
+# l.sort()
+# print(l)
+
+# l.reverse()
+# print(l)
+
+# print(l.index(4))
+
+# print(l.count(1))
+
+# m=l.copy()
+# m[0]=0
+# print(l)
+# print(m)
+
+# l.insert(1,899)
+# print(l)
+
+m=[900,1000,1100]
+l.extend(m)
+print(l)
