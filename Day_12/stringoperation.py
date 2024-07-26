eae9dd9fc164db93d3b4bc433f488101715629bd
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-07-26 23:38:18 +0530

    String Operations

---
+++ Day_12/stringoperation.py
@@ -0,0 +1,21 @@
+# Strings Slicing and Operations on Strings in Python | Python Tutorial - Day #12
+
+# https://www.youtube.com/watch?v=8jW7lpT8HW8
+
+
+#slicing
+names="Gokul,Raktate"
+print(names[0:5])  #this will print from 0 to 4th position 
+print(names[:5])   #this will print from 0 to 4th position 
+print(names[3:5])  #this will print from 3 to 4th position 
+print(names[:])    #this will print everything
+
+#negative slicing
+print(names[:-3])    #this will print till lenth-3 char
+print(names[-3:-1])    #this will print from lenth-3 to lenth-1 
+
+
+#to find lenth of string
+print(len(names))
+
+
