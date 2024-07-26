3ebeb1638b7775cfe0bf107b85d1be77e980d7cc
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-07-26 23:32:04 +0530

    Comments,escape sequence and Print Statements

---
+++ Day_05/main.py
@@ -0,0 +1,29 @@
+# Comments, Escape Sequences & Print Statement | Python Tutorial - Day #5
+
+# https://www.youtube.com/watch?v=qxPMmW93eDs
+
+print("Hey, I am a good boy \n and this viewer is also a good \n boy/girl")
+
+# \n => Escape Sequence Character(new line character)
+
+# Comment
+
+# print("Hello World")
+
+'''
+Multi 
+-Line
+Comment
+'''
+
+"""
+Multi 
+-Line
+Comment
+"""
+
+print("Hey, I am a \"good boy\" \nand this viewer is also a good \n boy/girl") #\" any content.....\" >> displayed in double quotes 
+
+print("Hey", 6, 7, sep="~",end="009\n") #seperated each element by ~
+
+print("Gokul")
