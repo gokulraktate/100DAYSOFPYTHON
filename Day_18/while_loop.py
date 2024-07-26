a5e04eeddebf6b3caabfcaa6c292aedfefef5e1e
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-07-26 23:41:26 +0530

    While Loop

---
+++ Day_18/while_loop.py
@@ -0,0 +1,37 @@
+# While Loops in Python | Python Tutorial - Day #18
+
+# https://www.youtube.com/watch?v=-tCFyIyKVx0
+
+
+i=0
+while(i<=3):
+    print(i)
+    i=i+1
+
+print("Done with the loop")
+
+
+
+i=int(input("enter the number:"))
+while(i<=30):    
+    print(i)
+    
+print("Done with the loop")
+
+
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("I am inside else")
+
+
+# do while loop not work in python => execute atleast 1 time 
+'''
+
+do{
+    # loop body
+}while(condition);
+
+'''
