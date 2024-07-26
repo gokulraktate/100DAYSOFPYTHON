878295a99035a2697f05c7c781d09479d3da597a
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-07-26 23:36:38 +0530

    User Input

---
+++ Day_10/user_input.py
@@ -0,0 +1,17 @@
+#var=input()
+
+a=input("Enter your name:")
+print("my name is:",a)
+
+x=int(input("Enter first no:"))
+y=int(input("Enter second no:"))
+
+print(x+y)
+
+#or
+
+x=input("Enter first no:")
+y=input("Enter second no:")
+
+print(int(x)+int(y))
+
