1ce3cc15668ab36eed76dc1bcfef3b2ad2af3804
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-08-09 21:35:08 +0530

    if __name__=="__main__" in Python

---
+++ Day_45/main.py
@@ -0,0 +1,8 @@
+# if __name__ == "__main__" in Python | Python Tutorial - Day #45
+
+# https://www.youtube.com/watch?v=y_CX2Rvitk8&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=45
+
+
+import harry
+
+harry.welcome()
